84e1c6e1e26a78dd4bd6441cf48e4af9703c393b
Kate <c.chistova2014@yandex.ru>
2022-10-17 22:32:41 +0300

    Последняя цифра трехзначного числа

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(99 < num && num < 1000)
+{
+    Console.Write("Последняя цифра вашего числа - " + (num % 10));
+}
+else
+{
+    Console.Write("Упс, вы ввели неверное число :( ");
+}
