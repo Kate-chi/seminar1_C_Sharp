d70373ceb384986c80f8f4b55397988bf14ad677
Kate <c.chistova2014@yandex.ru>
2022-10-23 18:20:31 +0300

    является ли одно число квадратом другого

---
+++ Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Одно число не является квадратом другого");
+}
