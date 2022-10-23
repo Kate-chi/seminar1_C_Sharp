cd6c8e3556a8015404d2970649d480432265fe99
Kate <c.chistova2014@yandex.ru>
2022-10-23 18:10:02 +0300

    кратно ли первое число второму

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int remainder = num1 % num2;
+
+if(remainder == 0)
+{
+    Console.Write($"{num1} кратно {num2}");
+}
+else
+{
+    Console.Write($"Остаток от деления {num1} на {num2} = {remainder}");
+}
