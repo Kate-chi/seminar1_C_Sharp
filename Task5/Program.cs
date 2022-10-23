4f4bfd9c2ba7153f2d2ebeaf6548ce92548c98e8
Kate <c.chistova2014@yandex.ru>
2022-10-23 17:38:25 +0300

    наибольшая цифра числа

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿int randomNum = new Random().Next(10, 100);
+Console.WriteLine(randomNum);
+
+int firstNum = randomNum / 10;
+int secondNum = randomNum % 10;
+
+Console.Write($"Наибольшая цифра числа: {FindMax(firstNum, secondNum)}");
+
+int FindMax(int num1, int num2)
+{
+    if(num1 > num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
