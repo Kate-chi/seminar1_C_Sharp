1a693dc19693c798a29c27b08f0ea460a2a496d0
Kate <c.chistova2014@yandex.ru>
2022-10-27 18:18:04 +0300

    В какой четверти находится точка по координатам

---
+++ Task10/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты точки (X и Y),
+ причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+ в которой находится эта точка.
+*/
+
+int x = InputNum("Введите координаты Х: ");
+int y = InputNum("Введите координаты Y: ");
+
+if(x == 0 || y == 0)
+{
+    Console.WriteLine("Упс, вы ввели неправильное число :( ");
+    return;
+}
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в первой четверти");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится во второй четверти");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в третьей четверти");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в четвертой четверти");
+}
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
