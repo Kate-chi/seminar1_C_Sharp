8c154779df6a6cfefcffc540a1d4113e8f22403a
Kate <c.chistova2014@yandex.ru>
2022-11-27 19:34:10 +0300

    2D массив, заполение и вывод методы

---
+++ Task22/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задайте двумерный массив M x N, каждый элемент которого находится по формуле: Amn = M + N
+
+m = 3; n = 4;
+
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+
+int[,] array2D = new int[rows, columns];
+
+Fill2DArr(array2D);
+Print2DArr(array2D);
+
+void Fill2DArr(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+}
+
+void Print2DArr(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
